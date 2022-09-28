fc8474c52c602d3629996ba4d2b1f37d0a8ce135
Lucas <luscaalima81@gmail.com>
2022-09-28 17:44:59 -0300

    adding sendSMS method

--- classes.py
+++ classes.py
@@ -1,24 +1,43 @@
 import smtplib
 import email.message
+from twilio.rest import Client
+
 
 
 class Pessoa:
-    def __init__(self, nome, idade, email):
+    # Construtor 
+    def __init__(self, nome, idade, email,telefone):
         self.nome = nome
         self.idade = idade
         self.email = email
-
+        self.telefone = telefone
     def metodo1(self):
         # RETORNA UMA STRING( CADEIA DE CARACTERES)
         return "EXECUTA O MÉTODO 1 DA CLASSE PESSOA"
+
+    # def sendSMS(self,sms:str):
+    #      # Your Account SID from twilio.com/console
+    #     account_sid = "AC4e10f85f5bca58d38e8ef2a20b2b8d15"
+    #     # Your Auth Token from twilio.com/console
+    #     auth_token  = "720bf2f4a0d874c4976998a9ce2bbd9e"
+    #     client = Client(account_sid, auth_token)
+    #     message = client.messages.create(
+    #     to=self.telefone, 
+    #     from_="+18149925691",
+    #     body=sms)
+    #     print("message.sid ->",message.sid)  
+    #     return f'{sms} enviado'
 
 
 # Herança
 # A classes filha (Funcionario) só está repassando os  dados de nome,idade e email para a classe Pai(Pessoa) através do super()
 class Funcionario(Pessoa):
-    def __init__(self, nome, idade, email, idFuncionario):
-        self.idFuncionario = idFuncionario
-        super().__init__(nome, idade, email)
+    def __init__(self, nome, idade, email,telefone, idFuncionario):
+        if idFuncionario == 'sas':
+         self.idFuncionario = 'idFuncionario'
+        else :
+            self.idFuncionario ="lucas " 
+        super().__init__(nome, idade, email,telefone)
 
     # POLIMORFISMO SOBREESCREVENDO UM MÉTODO
     def metodo1(self):
@@ -29,21 +48,21 @@ class Funcionario(Pessoa):
     #       return f'enviar email para{people.email}'
 
     # def send_email(self, people: Pessoa):
-        corpo_email = """
-            <img src="imagem.jpg">
-        """
+    #     corpo_email = """
+    #         <img src="imagem.jpg">
+    #      """
 
-        msg = email.message.Message()
-        msg["Subject"] = "Assunto"
-        msg["From"] = "luscaalima81@gmail.com"
-        msg["To"] = people.email
-        password = "fgulupzxbpnqembd"
-        msg.add_header("Content-Type", "text/html")
-        msg.set_payload(corpo_email)
+    #     msg = email.message.Message()
+    #     msg["Subject"] = "Assunto"
+    #     msg["From"] = "luscaalima81@gmail.com"
+    #     msg["To"] = people.email
+    #     password = "fgulupzxbpnqembd"
+    #     msg.add_header("Content-Type", "text/html")
+    #     msg.set_payload(corpo_email)
 
-        s = smtplib.SMTP("smtp.gmail.com: 587")
-        s.starttls()
-        # Login Credentials for sending the mail
-        s.login(msg["From"], password)
-        s.sendmail(msg["From"], [msg["To"]], msg.as_string().encode("utf-8"))
-        return f"Email enviado para {people.email}"
+    #     s = smtplib.SMTP("smtp.gmail.com: 587")
+    #     s.starttls()
+    #     # Login Credentials for sending the mail
+    #     s.login(msg["From"], password)
+    #     s.sendmail(msg["From"], [msg["To"]], msg.as_string().encode("utf-8"))
+    #     return f"Email enviado para {people.email}"
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from classes import Pessoa,Funcionario
 
 
-p1 = Pessoa('PESSOA 01',21,'luscaalima81@gmail.com')
-f1 = Funcionario('PESSOA 02',21,'luscaalima81@gmail.com','+1 atributo para classe funcioario')
-# print(p1)
-# print(f1.send_email(p1))
+p1 = Pessoa('PESSOA 01',21,'thiago.escudero@yahoo.com.br','+5519988648257')
+f1 = Funcionario('PESSOA 02',21,'luscaalima81@gmail.com','+1 atributo para classe funcioario','sas')
+print(p1.sendSMS('haahahah'))
+# print(p1.__repr__())
+
+
+# def flodarEmail():
+#  for i in range(10):
+#     print(f'email - {i} ',f1.send_email(p1))
+
+# flodarEmail()

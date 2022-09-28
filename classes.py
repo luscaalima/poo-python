b79e15e89044f1ea33baf646efc49264f7b52882
Lucas <luscaalima81@gmail.com>
2022-09-28 13:59:31 -0300

    polymorphism

--- classes.py
+++ classes.py
@@ -3,10 +3,17 @@ class Pessoa :
        self.nome = nome  
        self.idade = idade
        self.email = email
-    
-    
+       
+    def metodo1(self):
+        # RETORNA UMA STRING( CADEIA DE CARACTERES)
+        return 'EXECUTA O MÉTODO 1 DA CLASSE PESSOA' 
 #Herança    
+#A classes filha (Funcionario) só está repassando os  dados de nome,idade e email para a classe Pai(Pessoa) através do super() 
 class Funcionario(Pessoa):
        def __init__(self, nome, idade,email,idFuncionario):
         self.idFuncionario = idFuncionario
         super().__init__(nome, idade,email)
+       #POLIMORFISMO SOBREESCREVENDO UM MÉTODO
+       def metodo1(self):
+        # RETORNA UMA STRING( CADEIA DE CARACTERES)
+        return 'EXECUTA O MÉTODO 1 DA CLASSE FUNCIONARIO'     
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from classes import Pessoa,Funcionario
 p1 = Pessoa('Lucass',21,'luscaalima81@gmail.com')
 f1 = Funcionario('Lucass',21,'luscaalima81@gmail.com','+1 atributo para classe funcioario')
 # print(p1)
-print(f1.idFuncionario)
+print(f1.metodo1())

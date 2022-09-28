ed40d347bc74399d231bacd1b307bc880119f107
Lucas <luscaalima81@gmail.com>
2022-09-28 14:39:56 -0300

    sending email Employee -> Person

--- classes.py
+++ classes.py
@@ -1,19 +1,49 @@
-class Pessoa : 
-    def __init__(self,nome,idade,email):
-       self.nome = nome  
-       self.idade = idade
-       self.email = email
-       
+import smtplib
+import email.message
+
+
+class Pessoa:
+    def __init__(self, nome, idade, email):
+        self.nome = nome
+        self.idade = idade
+        self.email = email
+
     def metodo1(self):
         # RETORNA UMA STRING( CADEIA DE CARACTERES)
-        return 'EXECUTA O MÉTODO 1 DA CLASSE PESSOA' 
-#Herança    
-#A classes filha (Funcionario) só está repassando os  dados de nome,idade e email para a classe Pai(Pessoa) através do super() 
+        return "EXECUTA O MÉTODO 1 DA CLASSE PESSOA"
+
+
+# Herança
+# A classes filha (Funcionario) só está repassando os  dados de nome,idade e email para a classe Pai(Pessoa) através do super()
 class Funcionario(Pessoa):
-       def __init__(self, nome, idade,email,idFuncionario):
+    def __init__(self, nome, idade, email, idFuncionario):
         self.idFuncionario = idFuncionario
-        super().__init__(nome, idade,email)
-       #POLIMORFISMO SOBREESCREVENDO UM MÉTODO
-       def metodo1(self):
+        super().__init__(nome, idade, email)
+
+    # POLIMORFISMO SOBREESCREVENDO UM MÉTODO
+    def metodo1(self):
         # RETORNA UMA STRING( CADEIA DE CARACTERES)
-        return 'EXECUTA O MÉTODO 1 DA CLASSE FUNCIONARIO'     
+        return "EXECUTA O MÉTODO 1 DA CLASSE FUNCIONARIO"
+
+    #   def sendEmail(self,people:Pessoa):
+    #       return f'enviar email para{people.email}'
+
+    # def send_email(self, people: Pessoa):
+        corpo_email = """
+            <img src="imagem.jpg">
+        """
+
+        msg = email.message.Message()
+        msg["Subject"] = "Assunto"
+        msg["From"] = "luscaalima81@gmail.com"
+        msg["To"] = people.email
+        password = "fgulupzxbpnqembd"
+        msg.add_header("Content-Type", "text/html")
+        msg.set_payload(corpo_email)
+
+        s = smtplib.SMTP("smtp.gmail.com: 587")
+        s.starttls()
+        # Login Credentials for sending the mail
+        s.login(msg["From"], password)
+        s.sendmail(msg["From"], [msg["To"]], msg.as_string().encode("utf-8"))
+        return f"Email enviado para {people.email}"
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from classes import Pessoa,Funcionario
 
 
-p1 = Pessoa('Lucass',21,'luscaalima81@gmail.com')
-f1 = Funcionario('Lucass',21,'luscaalima81@gmail.com','+1 atributo para classe funcioario')
+p1 = Pessoa('PESSOA 01',21,'luscaalima81@gmail.com')
+f1 = Funcionario('PESSOA 02',21,'luscaalima81@gmail.com','+1 atributo para classe funcioario')
 # print(p1)
-print(f1.metodo1())
+# print(f1.send_email(p1))
